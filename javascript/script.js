2d92c67f6d116579064a508fa006882507d627d6
Ermakov Anton <ermakovantonsergeevich@yandex.ru>
2022-04-17 01:02:56 +0400

    javascript

--- javascript/script.js
+++ javascript/script.js
@@ -2,10 +2,34 @@ const popupOpenButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = popup.querySelector('.popup__close-button');
 
+let profileName = document.querySelector('.profile__name')
+let profileJob = document.querySelector('.profile__position')
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.popup__input-name');
+let jobInput = document.querySelector('.popup__input-job');
+
 function popupOpenToggle(evt) {
     console.log("evt", evt);
     popup.classList.toggle('popup_visible')
 }
 
-popupOpenButton.addEventListener('click', popupOpenToggle);
 popupCloseButton.addEventListener('click', popupOpenToggle);
+
+function editProfile() {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+    popupOpenToggle();
+}
+
+popupOpenButton.addEventListener('click', editProfile);
+
+// Обработчик «отправки» формы,
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    popupOpenToggle();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
